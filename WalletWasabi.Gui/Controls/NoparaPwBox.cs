6c6e4e52f0b902cbdc890e2c5544cc3cabd5c868
molnard <molnardavid84@gmail.com>
2019-04-08 15:40:07 +0200

    start to work with templates

---
+++ WalletWasabi.Gui/Controls/NoparaPwBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletWasabi.Gui.Controls
+{
+	public class NoparaPwBox : ExtendedTextBox
+    {
+		static NoparaPwBox()
+		{
+			//FocusableProperty.OverrideDefaultValue(typeof(NoparaPwBox), true);
+		}
+		public NoparaPwBox()
+		{
+
+		}
+	}
+}
